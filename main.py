5ed5bcfb54fefc4ffbe7d0e46edcc7332955d568
hdensten <hannah.densten@gmail.com>
2019-09-23 13:55:39 -0600

    Finished navigation, made bet amount player input

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-player_wallet = 100
+player_wallet = 0
 
 def add_to_wallet():
     global player_wallet
@@ -22,15 +22,16 @@ slot_odds = {
     'Pot of Gold': 1
 }
 
-def pull_the_lever(slot_odds):
+def pull_the_lever(slot_odds, bet):
     global player_wallet
     # global slot_odds
-    player_wallet -= 10
+    player_wallet -= int(bet)
     reel_one = random.choices(list(slot_odds.keys()), list(slot_odds.values()))
     reel_two = random.choices(list(slot_odds.keys()), list(slot_odds.values()))
     reel_three = random.choices(list(slot_odds.keys()), list(slot_odds.values()))
     reel_result = [reel_one[0], reel_two[0], reel_three[0]]
     print(reel_result)
+    winner_result(reel_result)
     return reel_result
 
 # reel_result = pull_the_lever(slot_odds)
@@ -42,23 +43,25 @@ def winner_result(reel_result):
         print('Partial Winner')
     else:
         print('Loser')
+    add_winnings_to_wallet(reel_result, slot_odds, bet)
 
 # winner_result(reel_result)
 
-def add_winnings_to_wallet(reel_result, slot_odds):
+def add_winnings_to_wallet(reel_result, slot_odds, bet):
     global player_wallet
     if reel_result[0] == reel_result[1] and reel_result[1] == reel_result[2]:
-        winnings = (1 / slot_odds[reel_result[0]]) * 1000
+        winnings = (1 / slot_odds[reel_result[0]]) * 100 * int(bet)
         player_wallet += winnings
         print(f'Congradulations! You have won ${winnings:.2f}')
     elif reel_result[0] == reel_result[1] or reel_result[0] == reel_result[2]:
-        winnings = (1 / slot_odds[reel_result[0]]) * 200
+        winnings = (1 / slot_odds[reel_result[0]]) * 20 * int(bet)
         player_wallet += winnings
         print(f'Congradulations! You have won ${winnings:.2f}')
     elif reel_result[1] == reel_result[2]:
-        winnings = (1 / slot_odds[reel_result[1]]) * 200
+        winnings = (1 / slot_odds[reel_result[1]]) * 20 * int(bet)
         player_wallet += winnings
         print(f'Congradulations! You have won ${winnings:.2f}')
+    navigation()
     
 
 # add_winnings_to_wallet(reel_result, slot_odds)
@@ -66,10 +69,16 @@ def add_winnings_to_wallet(reel_result, slot_odds):
 # print(player_wallet)
 
 def navigation():
+    global player_wallet
     player_action = input(f'Would you like to... \n 1 : Play again \n 2 : Chash out and leave \n')
     if player_action == '1':
-        reel_result = pull_the_lever(slot_odds)
+        reel_result = pull_the_lever(slot_odds, bet)
+    elif player_action == '2':
+        print(f'${player_wallet:.2f} cashed out, goodbye!')
+
 
 print('Welcome to the Bottega Casino! Add money to your wallet to get started.')
 add_to_wallet()
-navigation()
+bet = input('Set the bet amount:   ')
+# navigation()
+reel_result = pull_the_lever(slot_odds, bet)
