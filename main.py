bf1d891b3bff3f732a9685e7b0c3ce5ad89defc5
hdensten <hannah.densten@gmail.com>
2019-09-23 10:01:19 -0600

    Add Pip files, add slot_odds

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 player_wallet = 0
 
 def add_to_wallet():
@@ -7,3 +9,16 @@ def add_to_wallet():
     return player_wallet
 
 add_to_wallet()
+
+slot_odds = {
+    7: 3, 
+    777: 2, 
+    'Cherry': 10, 
+    'Apple': 10, 
+    'Orange': 10,
+    'Watermelon': 10,
+    'Pear': 10,
+    'Pineapple': 10,
+    'Bar': 2,
+    'Pot of Gold': 1
+}
